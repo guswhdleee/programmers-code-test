3dc44ea32c3665346afd55cf83c245a70413819d
guswhdleee <gksrmrtn123@naver.com>
2024-09-02 16:34:11 +0900

    09.02 문제풀이 추가

---
+++ lv0/9로 나눈 나머지.js
@@ -0,0 +1,8 @@
+function solution(number) {
+    var answer = 0;
+    for(i=0; i< number.length; i++) {
+        answer += parseInt(number[i]);
+    }
+    answer = answer % 9
+    return answer;
+}
