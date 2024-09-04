66acf4409f7bf7f212404c5f58d6ce04c40751f9
guswhdleee <gksrmrtn123@naver.com>
2024-09-04 16:28:25 +0900

    '09.04일 문제 추가'
    09.04일 문제 추가
    
    09.04 문제추가

--- lv0/9로 나눈 나머지.js
+++ lv0/9로 나눈 나머지.js
@@ -1,8 +1,8 @@
 function solution(number) {
-    var answer = 0;
-    for(i=0; i< number.length; i++) {
-        answer += parseInt(number[i]);
-    }
-    answer = answer % 9
-    return answer;
+  var answer = 0;
+  for (i = 0; i < number.length; i++) {
+    answer += parseInt(number[i]);
+  }
+  answer = answer % 9;
+  return answer;
 }
