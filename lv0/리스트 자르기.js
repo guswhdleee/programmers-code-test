e0cfd076abfade2eb76b7634a513806a2c4f82cb
guswhdleee <gksrmrtn123@naver.com>
2024-09-10 17:26:01 +0900

    리스트 자르기 문제 추가

---
+++ lv0/리스트 자르기.js
@@ -0,0 +1,18 @@
+function solution(n, slicer, num_list) {
+  var answer = [];
+  var a = slicer[0];
+  var b = slicer[1];
+  var c = slicer[2];
+  if (n == 1) {
+    answer = num_list.slice(0, b + 1);
+  } else if (n == 2) {
+    answer = num_list.slice(a);
+  } else if (n == 3) {
+    answer = num_list.slice(a, b + 1);
+  } else if (n == 4) {
+    for (i = a; i <= b; i += c) {
+      answer.push(parseInt(num_list.slice(i, i + 1)));
+    }
+  }
+  return answer;
+}
