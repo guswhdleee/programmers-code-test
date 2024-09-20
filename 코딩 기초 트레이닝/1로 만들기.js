7f13124a9f8aaa36191d9a89fd7b5d4b3fb33a20
guswhdleee <gksrmrtn123@naver.com>
2024-09-20 17:07:44 +0900

    1로 만들기

---
+++ 코딩 기초 트레이닝/1로 만들기.js
@@ -0,0 +1,15 @@
+function solution(num_list) {
+  var answer = 0;
+  for (value of num_list) {
+    while (value != 1) {
+      if (value % 2 == 0) {
+        value = value / 2;
+        answer++;
+      } else if (value % 2 == 1) {
+        value = (value - 1) / 2;
+        answer++;
+      }
+    }
+  }
+  return answer;
+}
